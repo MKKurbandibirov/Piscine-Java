084ffdad25af29796ae21bf227f8ceb81933e66a
Napoleon Farfetch <nfarfetc@va-c5.kzn.21-school.ru>
2022-07-18 09:36:46 +0300

    Add ex00

---
+++ Day_00/ex00/Program.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Program {
+
+    public static void main(String[] args) {
+        int value = 479598;
+        int result = 0;
+
+        while (value > 0) {
+            result += value % 10;
+            value /= 10;
+        }
+        System.out.println(result);
+    }
+}
