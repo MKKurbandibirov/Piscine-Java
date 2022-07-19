7b520fb24ae3f5da1f4ba4fefe86bc368e77a4e6
Napoleon Farfetch <nfarfetc@va-c5.kzn.21-school.ru>
2022-07-19 13:02:12 +0300

    Fix one bug

--- Day_00/ex01/Program.java
+++ Day_00/ex01/Program.java
@@ -5,16 +5,12 @@ public class Program {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int value = input.nextInt();
-        int sqrt = 1;
         int i = 2;
         if (value < 0 || value == 0 || value == 1) {
             System.err.println("IllegalArgument");
             System.exit(1);
         }
-        while (sqrt * sqrt < value) {
-            sqrt++;
-        }
-        for (; i < sqrt; i++) {
+        for (; i * i <= value; i++) {
             if (value % i == 0) {
                 System.out.printf("%b %d\n", false, i - 1);
                 input.close();
