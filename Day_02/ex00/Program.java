1f8f32599063beaf8341ecb2973d43f709531e72
Napoleon Farfetch <nfarfetc@va-c5.kzn.21-school.ru>
2022-07-20 19:20:03 +0300

    Add Day02

---
+++ Day_02/ex00/Program.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Program {
+
+	private static Map<String, String[]> signatureMap;
+	private static final String SIGNATURE_PATH = "signature.txt";
+	private static final String RESULT_PATH = "result.txt";
+	private static LinkedList<String> resultList;
+	private static int maxLength = 0;
+
+	public static void setSignatureMap() {
+		try (FileInputStream fileInput = new FileInputStream(SIGNATURE_PATH)) {
+			signatureMap = new HashMap<>();
+			int length = (int)new File(SIGNATURE_PATH).length();
+			byte[] bytes = new byte[length];
+			fileInput.read(bytes);
+			String signatureLines = new String(bytes);
+			String[] splitedLines = signatureLines.split("\n");
+			for (String line : splitedLines) {
+				String[] keyVal = line.split(",");
+				String[] values = keyVal[1].trim().split(" ");
+				signatureMap.put(keyVal[0], values); 
+			}
+		} catch (IOException e) {
+			System.err.println("File not found!");
+		}
+	}
+
+	public static void setResultList() {
+		resultList = new LinkedList<>();
+		Scanner input = new Scanner(System.in);
+		String filePath;
+		while (true) {
+			if (!input.hasNextLine()) {
+				input.close();
+			}
+			filePath = input.nextLine();
+			if (filePath.equals("42")) {
+				break;
+			}
+			try (FileInputStream fileInput = new FileInputStream(filePath)) {
+				byte[] bytes = new byte[maxLength];
+				for (int i = 0; i < maxLength; i++) {
+					bytes[i] = (byte)fileInput.read();
+				}
+				String[] strBytes = new String[maxLength];
+				for (int i = 0; i < maxLength; i++) {
+					strBytes[i] = Integer.toHexString(bytes[i] & 0xff);
+					if (strBytes[i].length() == 1) {
+						strBytes[i] = "0" + strBytes[i].toUpperCase();
+					} else {
+						strBytes[i] = strBytes[i].toUpperCase();
+					}
+				}
+				boolean check = false;
+				for (Map.Entry<String, String[]> entry : signatureMap.entrySet()) {
+					check = true;
+					for (int i = 0; i < maxLength; i++) {
+						if (!entry.getValue()[i].equals(strBytes[i])) {
+							check = false;
+							break;
+						} 
+					}
+					if (check) {
+						System.out.println("PROCESSED");
+						resultList.add(entry.getKey());
+						break;
+					}
+				}
+				if (!check) {
+					System.out.println("UNDEFINED");
+				}
+			} catch (IOException e) {
+				System.err.println("File not found!");
+			}
+		}
+		input.close();
+	}
+
+	public static void main(String[] args) {
+		setSignatureMap();
+		for (Map.Entry<String, String[]> entry : signatureMap.entrySet()) {
+			System.out.printf("%s\t", entry.getKey());
+			for (String string : entry.getValue()) {
+				System.out.printf("%s ", string);
+			}
+			System.out.println();
+		}
+
+
+		for (String[] values : signatureMap.values()) {
+			if (values.length > maxLength) {
+				maxLength = values.length;
+			}
+		}
+		setResultList();
+		try (FileOutputStream fileInput = new FileOutputStream(RESULT_PATH)) {
+			for (String string : resultList) {
+				byte[] bytes = string.getBytes();
+				fileInput.write(bytes, 0, bytes.length);
+				byte[] nl = "\n".getBytes();
+				fileInput.write(nl);
+			}
+		} catch (IOException e) {
+			System.err.println("Don't have access");
+		}
+	}
+}
