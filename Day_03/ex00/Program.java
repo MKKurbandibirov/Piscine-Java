8969fbb73760916c2c5b546ab20b9c06a9e4be1b
Napoleon Farfetch <nfarfetc@un-r4.kzn.21-school.ru>
2022-07-21 18:18:35 +0300

    Add ex01

---
+++ Day_03/ex00/Program.java
@@ -0,0 +1,57 @@
+public class Program {
+	private static int count;
+
+	public static class Egg extends Thread {
+		@Override
+		public void run() {
+			for (int i = 0; i < count; i++) {
+				System.out.println("Egg");
+				try {
+					Thread.sleep(0);
+				} catch (InterruptedException e) {
+					System.err.println("Egg interrupted");
+				}
+			}
+		}
+	}
+
+	public static class Hen extends Thread {
+		@Override
+		public void run() {
+			for (int i = 0; i < count; i++ ) {
+				System.out.println("Hen");
+				try {
+					Thread.sleep(0);
+				} catch (InterruptedException e) {
+					System.err.println("Hen interrupted");
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		if(args.length == 1){
+			String[] tmp = args[0].split("=");
+			if (tmp[0].equals("--count")) {
+				count = Integer.parseInt(tmp[1]);
+				Thread egg = new Egg();
+				Thread hen = new Hen();
+				egg.start();
+				hen.start();
+				try {
+					egg.join();
+					hen.join();
+				} catch (InterruptedException e) {
+					e.getStackTrace();
+				}
+				for (int i = 0; i < count; i++) {
+					System.out.println("Human");
+				}
+			} else {
+				System.err.println("Invalid argument");
+			}
+		} else {
+			System.err.println("Invalid argument");
+		}
+	}
+}
---
+++ Day_03/ex01/Program.java
@@ -0,0 +1,78 @@
+public class Program {
+	private static int count;
+
+	public static class Printer {
+		public static boolean isPrinted = false;
+		public synchronized void print(String message) {
+			if (!isPrinted) {
+				try {
+					isPrinted = true;
+					wait();
+				} catch (InterruptedException e) {
+					e.getStackTrace();
+				}
+			}
+			notify();
+			isPrinted = false;
+			System.out.println(message);
+		}
+	}
+
+	public static class Egg extends Thread {
+		private String message;
+		private Printer printer;
+
+		public Egg(String message, Printer printer) {
+			this.message = message;
+			this.printer = printer;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i < count; i++) {
+				printer.print(message);
+			}
+		}
+	}
+
+	public static class Hen extends Thread {
+		private String message;
+		private Printer printer;
+
+		public Hen(String message, Printer printer) {
+			this.message = message;
+			this.printer = printer;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i < count; i++) {
+				printer.print(message);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		if(args.length == 1){
+			String[] tmp = args[0].split("=");
+			if (tmp[0].equals("--count")) {
+				count = Integer.parseInt(tmp[1]);
+				Printer printer1 = new Printer();
+				Thread hen = new Hen("Hen", printer1);
+				Thread egg = new Egg("Egg", printer1);
+				hen.start();
+				egg.start();
+				try {
+					egg.join();
+					hen.join();
+				} catch (InterruptedException e) {
+					e.getStackTrace();
+				}
+			} else {
+				System.err.println("Invalid argument");
+			}
+		} else {
+			System.err.println("Invalid argument");
+		}
+	}
+}
